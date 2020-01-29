d4d8c68d0be33d4369ec8cbefc6318760f0051d1
Kiss my ass <akifumifukatsu@Akifumis-MacBook-Air.local>
2020-01-29 21:17:58 +0900

    dataContorol added

---
+++ csvContorol/csvDataContoroller.py
@@ -0,0 +1,277 @@
+import time, calendar, os, sys, glob
+import json
+from datetime import datetime, timedelta
+from argparse import ArgumentParser
+
+import urllib.request
+from io import StringIO
+
+import gzip
+
+sys.path.append("../util")
+import util
+
+class CsvDataContoroller():
+    
+    def __init__(self,path,fileName):
+
+        self.PATH = path
+        if self.PATH is None:
+            self.PATH = "../dataOfBitstamp"
+        self.FILE_NAME = fileName
+        if self.FILE_NAME is None:
+            self.FILE_NAME = "bitstampUSD.csv"
+
+    def downloadDataFromAPI(self, url, filePath):
+
+        
+        try:
+            """
+            with urllib.request.urlopen(url) as web_file:
+                data = web_file.read()
+                with open(filePath, mode='wb') as local_file:
+                    local_file.write(data)
+
+            with gzip.open(filePath, mode='rt') as fp:
+                 with open(self.FILE_NAME, 'w') as f:
+                     f.write(fp.read())
+            """
+            pass
+        except urllib.error.URLError as e:
+            print(e)
+
+
+    def getBitHistory(self):
+    
+        DAY = 86400
+
+        term = DAY
+
+        with open(self.FILE_NAME,'r') as f: 
+            line = None
+            while True:
+                if(line is None): line = f.readline()
+                if(line is None or line == ''): break
+                data = []
+                data.append(line)
+                startDate = data[0].split(",")[0]
+                endDate = util.getDateFirstTime(startDate) + term
+                startDate = datetime.fromtimestamp(int(startDate))
+                filename = "bitCoinHisOfBitStamp_" + util.timeRegex(str(startDate)) + ".csv"
+                path = os.path.join(self.PATH, filename)
+
+                while True:
+                    line = f.readline()
+                    if line is None or line == '' or int(line.split(",")[0]) > endDate: break
+                    data.append(line)
+
+                util.write(path,data)
+                print("file made:", filename)
+
+    def makeOHLC(self,directory,file_list,DATE_FORMAT,TERM):
+        
+        if directory is None:
+            directory = self.PATH
+
+        MINUTES = 60
+        filenameSufix = ""
+        PATH = ""
+        term = 0
+        partisionMax = 0
+
+        if TERM == "1minute":
+            filenameSufix = "OHLC_1minute.csv"
+            term = MINUTES
+            PATH = "../dataOfBitstampOHLC/1minute"
+            if not os.path.isdir(PATH):
+                print("PATH:",PATH,"does not exist")
+                return
+
+        elif TERM == "5minutes":
+            filenameSufix = "OHLC_5minutes.csv"
+            term = 5 * MINUTES
+            PATH = "../dataOfBitstampOHLC/5minutes"
+            if not os.path.isdir(PATH):
+                print("PATH:",PATH,"does not exist")
+                return
+        
+        elif TERM == "15minutes":
+            filenameSufix = "OHLC_15minutes.csv"
+            term = 15 * MINUTES
+            PATH = "../dataOfBitstampOHLC/15minutes"
+            if not os.path.isdir(PATH):
+                print("PATH:",PATH,"does not exist")
+                return
+
+        elif TERM == "30minutes":
+            filenameSufix = "OHLC_30minutes.csv"
+            term = 30 * MINUTES
+            PATH = "../dataOfBitstampOHLC/30minutes"
+            if not os.path.isdir(PATH):
+                print("PATH:",PATH,"does not exist")
+                return
+
+        elif TERM == "1hour":
+            filenameSufix = "OHLC_1hour.csv"
+            term = 60 * MINUTES
+            PATH = "../dataOfBitstampOHLC/1hour"
+            if not os.path.isdir(PATH):
+                print("PATH:",PATH,"does not exist")
+                return
+
+        elif TERM == "4hours":
+            filenameSufix = "OHLC_4hours.csv"
+            term = 240 * MINUTES
+            PATH = "../dataOfBitstampOHLC/4hours"
+            if not os.path.isdir(PATH):
+                print("PATH:",PATH,"does not exist")
+                return
+            
+
+        elif TERM == "1day":
+            filenameSufix = "OHLC_1day.csv"
+            term = 3600 * MINUTES
+            PATH = "../dataOfBitstampOHLC/1day"
+            if not os.path.isdir(PATH):
+                print("PATH:",PATH,"does not exist")
+                return
+            partisionMax = 7
+
+        else:
+            print("TERM invalid")
+            exit(0)
+        
+        startTime = 0
+        partision = 0
+        for file in file_list:
+            if partision == 0:
+                filename = file.split(".")[0]+filenameSufix
+                path = os.path.join(PATH, filename)
+                print(filename)
+                ohlc = ['time,open,high,low,close,volume\n']
+            FILE_NAME = os.path.join(directory, file)
+            
+            with open(FILE_NAME,'r') as f: 
+                line = None
+                while True:
+                    if(line is None): 
+                        try:
+                            line = f.readline()
+                            startTime = util.getDateFirstMinute(line.split(",")[0])
+                        except Exception as e:
+                            print(str(e))
+
+                    if(line is None or line == ''): break
+                    data = []
+                    data.append(line)
+                    endTime = startTime + term
+                    #startTime = datetime.fromtimestamp(int(startTime))
+                    #filename = "bitCoinHisOfBitStampOHLC_" + timeRegex(str(startTime)) + ".csv"
+                    #path = os.path.join(PATH, filename)
+            
+                    before = []
+                    while True:
+                        line = f.readline()
+                        if line is None or line == '' or int(line.split(",")[0]) > endTime: break
+                        data.append(line)
+
+                    before = self.countOHLC(data, before, endTime)
+                    ohlc.append(",".join(before)+"\n")
+                    startTime += term
+                partision += 1
+            if partision >= partisionMax or file == file_list[len(file_list)-1]:
+                
+                #print(ohlc[0])    
+                util.write(path,ohlc)
+                #print(ohlc)
+                ohlc=[]
+                partision=0
+
+    def countOHLC(self, data, before, endTime):
+
+        if data is None or data == []:
+            before[0] = endTime
+            #before[2] = before[3] = before[4] = str(0)
+            return before
+
+        min = 999999999999
+        max = 0
+        open = data[0].split(",")[1]
+        close = data[len(data)-1].split(",")[1]
+        amount = 0
+
+        for d in data:
+            price = float(d.split(",")[1])
+            if(price < min):
+                min = price
+            if(price > max):
+                max = price
+            amount += float(d.split(",")[2])
+        
+        endTime = datetime.fromtimestamp(int(endTime))
+        endTime = util.timeRegexToMinute(str(endTime))
+        return [str(endTime),str(open), str(max), str(min), str(close), str(amount)]
+
+
+    def get_option(self, dateFormat, fromDate, term):
+        argparser = ArgumentParser()
+        argparser.add_argument('-d', '--data-format', type=str,
+                            default=dateFormat,
+                            help='Specify format of date')
+        argparser.add_argument('-f', '--from-date', type=lambda s: datetime.strptime(s, '%Y-%m-%d'),
+                            default=fromDate,
+                            help='Specify first date to make')
+
+        argparser.add_argument('-t', '--term', type=str,
+                                default=term,
+                                help='Specify term to make data')
+        
+        return argparser.parse_args()
+
+    def getDirectoryFiles(self,directory,fromDate):
+
+        if directory is None:
+            directory = self.PATH
+        FILE = os.listdir(directory)
+        file_list = []
+        fromDateFile = "bitCoinHisOfBitStamp_" + util.timeRegex(str(fromDate)) + ".csv"
+        while True:
+            #fromDateFile = "bitCoinHisOfBitStamp_"+timeRegex(str(fromDate))+"OHLC.csv"
+            fromDateFile = "bitCoinHisOfBitStamp_" + util.timeRegex(str(fromDate)) + ".csv"
+            if fromDateFile not in FILE:
+                if fromDate > datetime.now():
+                    break
+                fromDate = fromDate + timedelta(days=1)
+                continue
+            
+            print(fromDateFile)
+            file_list.append(fromDateFile)
+            fromDate = fromDate + timedelta(days=1)
+
+        return file_list
+
+if __name__ == "__main__":
+
+    URL = "https://api.bitcoincharts.com/v1/csv/bitstampUSD.csv.gz"
+    #URL = "http://localhost:3000"
+
+    #This should be latter than get_option to get args
+    contoroller = CsvDataContoroller(None,None)
+    #contoroller.downloadDataFromAPI(URL, "./bitStampUSD.csv.gzip")
+    contoroller.getBitHistory()
+
+    #defaul items
+    DATE_FORMAT = "NORMAL"
+    FROM_DATE = datetime(2011, 9, 13)
+    TERM = "1minute"
+
+    #this should be called first
+    args = contoroller.get_option(DATE_FORMAT, FROM_DATE, TERM)
+    
+    fromDate = args.from_date
+    
+    file_list = contoroller.getDirectoryFiles(None,fromDate)
+
+    #Now DATE_FORMAT is not used
+    contoroller.makeOHLC(None, file_list, args.data_format, args.term)
+    print("File Making End")
---
+++ dbContorol/dbDataContoroller.py
@@ -0,0 +1,284 @@
+import time, calendar, os, sys, glob
+import json
+from datetime import datetime, timedelta
+from argparse import ArgumentParser
+import traceback
+
+import MySQLdb as sql
+
+sys.path.append("../util")
+import util
+
+
+class DbDataContoroller():
+    
+    def __init__(self,path,term):
+
+        self.TERM = term
+        if self.TERM is None:
+            self.TERM = "1minute"
+
+        self.PATH = path
+        if self.PATH is None:
+            self.PATH = "../dataOfBitstamp"
+
+        self.CONN = sql.connect(
+            host = 'localhost',
+            user = 'root',
+            password = 'american1911!',
+            database = 'trade'
+        )
+
+
+    def makeOHLC(self,directory,file_list,DATE_FORMAT,TERM):
+       
+        if directory is None:
+            directory = self.PATH
+
+        MINUTES = 60
+
+        term = 0
+        partisionMax = 0
+
+        if TERM == "1minute":
+            term = MINUTES
+            
+        elif TERM == "1hour":
+            term = 60 * MINUTES
+        
+        else:
+            print("TERM invalid")
+            exit(0)
+        
+        startTime = 0
+        partision = 0
+        for file in file_list:
+            
+            if partision == 0:
+                ohlc = []
+            
+            FILE_NAME = os.path.join(directory, file)
+            
+            with open(FILE_NAME,'r') as f: 
+                line = None
+                while True:
+                    if(line is None): 
+                        try:
+                            line = f.readline()
+                            startTime = util.getDateFirstMinute(line.split(",")[0])
+                        except Exception as e:
+                            print(str(e))
+
+                    if(line is None or line == ''): break
+                    data = []
+                    data.append(line)
+                    endTime = startTime + term
+                    #startTime = datetime.fromtimestamp(int(startTime))
+                    #filename = "bitCoinHisOfBitStampOHLC_" + timeRegex(str(startTime)) + ".csv"
+                    #path = os.path.join(PATH, filename)
+            
+                    before = []
+                    while True:
+                        line = f.readline()
+                        if line is None or line == '' or int(line.split(",")[0]) > endTime: break
+                        data.append(line)
+
+                    before = self.countOHLC(data, before, endTime)
+                    ohlc.append(before)
+                    startTime += term
+                partision += 1
+            if partision >= partisionMax or file == file_list[len(file_list)-1]:
+                
+                #print(ohlc[0])    
+                self.writeToDB(ohlc, TERM)
+                #util.write(path,ohlc)
+                #print(ohlc)
+                ohlc=[]
+                partision=0
+
+    def countOHLC(self, data, before, endTime):
+
+        if data is None or data == []:
+            before[0] = endTime
+            #before[2] = before[3] = before[4] = str(0)
+            return before
+
+        min = 999999999999
+        max = 0
+        open = data[0].split(",")[1]
+        close = data[len(data)-1].split(",")[1]
+        amount = 0
+
+        for d in data:
+            price = float(d.split(",")[1])
+            if(price < min):
+                min = price
+            if(price > max):
+                max = price
+            amount += float(d.split(",")[2])
+        
+        endTime = datetime.fromtimestamp(int(endTime))
+        endTime = util.timeRegexToMinute(str(endTime))
+        return [str(endTime),str(open), str(max), str(min), str(close), str(amount)]
+
+
+    def get_option(self, dateFormat, fromDate, term):
+        argparser = ArgumentParser()
+        argparser.add_argument('-d', '--data-format', type=str,
+                            default=dateFormat,
+                            help='Specify format of date')
+        argparser.add_argument('-f', '--from-date', type=lambda s: datetime.strptime(s, '%Y-%m-%d'),
+                            default=fromDate,
+                            help='Specify first date to make')
+
+        argparser.add_argument('-t', '--term', type=str,
+                                default=term,
+                                help='Specify term to make data')
+        
+        return argparser.parse_args()
+
+    def getDirectoryFiles(self,directory,fromDate):
+
+        if directory is None:
+            directory = self.PATH
+        FILE = os.listdir(directory)
+        
+        file_list = []
+        fromDateFile = "bitCoinHisOfBitStamp_" + util.timeRegex(str(fromDate)) + ".csv"
+        while True:
+            #fromDateFile = "bitCoinHisOfBitStamp_"+timeRegex(str(fromDate))+"OHLC.csv"
+            fromDateFile = "bitCoinHisOfBitStamp_" + util.timeRegex(str(fromDate)) + ".csv"
+            if fromDateFile not in FILE:
+                if fromDate > datetime.now():
+                    break
+                fromDate = fromDate + timedelta(days=1)
+                continue
+            
+            print(fromDateFile)
+            file_list.append(fromDateFile)
+            fromDate = fromDate + timedelta(days=1)
+
+        return file_list
+
+    def writeToDB(self, ohlc_list, term):
+        
+        table_name = "price_"
+        if term == "1minute":
+            table_name += "1min"
+
+        elif term == "1hour":
+            table_name += term
+        else:
+            print("TERM invalid")
+            exit(0)
+
+        cursor = self.CONN.cursor()
+        
+        for ohlc in ohlc_list:
+            try:
+                #print(ohlc)
+                #The latest data in DB should be replaced
+                #cursor.execute("INSERT INTO "+ table_name +" VALUES(%s, %s, %s, %s, %s, %s, %s, %s)", (ohlc[0], 1, 1, ohlc[1], ohlc[4], ohlc[2], ohlc[3], ohlc[5]))
+                pass
+            except:
+                traceback.print_exc()
+        self.CONN.commit()
+
+
+    def selectData(self, term, date):
+
+        
+        table_name = "price_"
+        if term == "1minute":
+            table_name += "1min"
+
+        elif term == "1hour":
+            table_name += term
+        else:
+            print("TERM invalid")
+            exit(0)
+
+        cursor = self.CONN.cursor()
+
+        try:
+            #query = "SELECT * FROM "+ table_name + " WHERE date = %s"
+            #print(query)
+            date = "'" + date + "'"
+            cursor.execute("SELECT * FROM "+ table_name + " WHERE date > " + date)
+            rows = cursor.fetchall()
+            
+            return rows
+
+            #exit(1)
+            #cursor.execute("DELETE FROM " + table_name + " WHERE date = " + ohlc[0][0])
+        except:
+            traceback.print_exc()
+            exit(1)
+
+    def deleteData(self, date, term):
+
+        table_name = "price_"
+        if term == "1minute":
+            table_name += "1min"
+
+        elif term == "1hour":
+            table_name += term
+        else:
+            print("TERM invalid")
+            exit(0)
+
+        cursor = self.CONN.cursor()
+
+        try:
+            date = "'" + date + "'"
+            cursor.execute("DELETE FROM "+ table_name + " WHERE date > " + date)
+            rows = cursor.fetchall()
+            
+            return rows
+
+        except:
+            traceback.print_exc()
+            exit(1)
+
+
+
+if __name__ == "__main__":
+
+    URL = "https://api.bitcoincharts.com/v1/csv/bitstampUSD.csv.gz"
+    #URL = "http://localhost:3000"
+
+    #This should be latter than get_option to get args
+    contoroller = DbDataContoroller(None,"1minute")
+    #contoroller.downloadDataFromAPI(URL, "./bitStampUSD.csv.gzip")
+    #contoroller.getBitHistory()
+
+    #defaul items
+    DATE_FORMAT = "NORMAL"
+    FROM_DATE = datetime(2011, 9, 13)
+    TERM = "1minute"
+
+    #this should be called first
+    args = contoroller.get_option(DATE_FORMAT, FROM_DATE, TERM)
+    
+    fromDate = args.from_date
+
+    file_list = contoroller.getDirectoryFiles(None,fromDate)
+
+    date = file_list[-1].split("_")[1].split(".")[0]
+
+    #print(date)
+
+    result = contoroller.selectData(args.term, date)
+    #print(result)
+    num = 0
+    if args.term == "1min":
+        num = 1441
+    elif args.term == "1hour":
+        num = 25
+
+    if len(result) < num:
+        contoroller.deleteData(date, args.term)
+
+    #Now DATE_FORMAT is not used
+    contoroller.makeOHLC(None, file_list, args.data_format, args.term)
+    print("File Making End")
